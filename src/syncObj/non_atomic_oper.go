54a954597cafd535d596bb16ce40813b721df145
chosm10 <pelto@naver.com>
2019-07-03 14:51:53 +0900

    연산이 원자성 있지 못하게 처리된 경우

---
+++ src/syncObj/non_atomic_oper.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	var data int32 = 0
+	wait_group := new(sync.WaitGroup)
+
+	for i := 0; i < 2000; i++ {
+		wait_group.Add(1)
+		go func() {
+			data += 1
+			wait_group.Done()
+		}()
+	}
+
+	for i := 0; i < 1000; i++ {
+		wait_group.Add(1)
+		go func() {
+			data -= 1
+			wait_group.Done()
+		}()
+	}
+
+	wait_group.Wait()
+	fmt.Println(data)
+}
